024843437c0155dff26ff71f1ee4d37e3d456de6
jrbeverly <jrbeverly@live.com>
2019-04-19 19:45:31 -0400

    Add example for using a module output

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,7 +1,21 @@
 module "example" {
   source = "../../"
-}
 
-output "rendered" {
-  value = "${module.example.rendered}"
+  bucket = "infraprints-terraform-state-output-example"
+  key    = "ref/aws/infraprints/outputs.tf"
+
+  outputs = [
+    {
+      key   = "s3_bucket"
+      value = "infraprints-terraform-state-output-example"
+    },
+    {
+      key   = "route53_hosted_zone"
+      value = "AFIA123412341234"
+    },
+    {
+      key   = "aws_account_id"
+      value = "123412341234"
+    },
+  ]
 }
