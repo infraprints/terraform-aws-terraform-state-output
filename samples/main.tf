a50e979cf47480e5810961b0ceff2904f1d4ccb3
jrbeverly <jrbeverly@live.com>
2019-04-19 21:43:01 -0400

    Add sample for project

---
+++ samples/main.tf
@@ -0,0 +1,21 @@
+module "output_resources" {
+  source = "git::https://gitlab.com/infraprints/modules/aws/terraform-state-output"
+
+  bucket = "infraprints-terraform-state-output"
+  key    = "aws/infraprints/project/outputs.tf"
+
+  terraform_output = [
+    {
+      key   = "s3_bucket"
+      value = "infraprints-terraform-state-output"
+    },
+    {
+      key   = "route53_hosted_zone"
+      value = "AFIA123412341234"
+    },
+    {
+      key   = "aws_account_id"
+      value = "123412341234"
+    },
+  ]
+}
