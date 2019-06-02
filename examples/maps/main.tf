4df23ea60503ce513abbba3e1d4ef6bcb551a0c7
jrbeverly <jrbeverly@live.com>
2019-06-02 19:19:31 -0400

    Simplify the variables

--- examples/maps/main.tf
+++ examples/maps/main.tf
@@ -13,16 +13,12 @@ module "example" {
 
   terraform_output = [
     {
-      key   = "s3_bucket"
-      value = "infraprints-terraform-state-output-example"
+      key   = "terraform_remote_output"
+      value = aws_s3_bucket.outputs.id
     },
     {
-      key   = "route53_hosted_zone"
-      value = "AFIA123412341234"
-    },
-    {
-      key   = "aws_account_id"
-      value = "123412341234"
+      key   = "name"
+      value = "project"
     },
     {
       key   = "tags"
